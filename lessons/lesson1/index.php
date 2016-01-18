9c9750a159814f42da523297558ecc7f5ed84c7b
shklovsky <s.shklovskyi@umberfirm.com>
2016-01-18 16:59:42 +0200

    #logout

--- lessons/lesson1/index.php
+++ lessons/lesson1/index.php
@@ -21,7 +21,7 @@ if (isset($_GET['logout'])) {
     <head>
         <meta charset="UTF-8">
         <title>Работа с сессией</title>
-
+        <link rel="stylesheet" href="/lessons/lesson1/css/style.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-1.11.3.min.js" type="text/javascript"></script>
     </head>
@@ -37,6 +37,17 @@ if (isset($_GET['logout'])) {
         </div>
 
         <div class="container">
+            <?php if (isset($_COOKIE['loggedIn']) && $_COOKIE['loggedIn']) { ?>
+                <div class="cookie-wrapper">
+                    <?php foreach ($_COOKIE as $name => $value) { ?>
+                        <div class="cookie-line"> 
+                            <div class=""><?= $name ?>:</div>
+                            <div class=""><?= $value ?></div>
+                        </div>
+                    <?php } ?>
+                </div>
+            <?php } ?>
+
             <div class="col-lg-5">
                 <form role="form" method="get" action="/lessons/lesson1/controller/auth.php" id="login-form">
 
@@ -66,6 +77,7 @@ if (isset($_GET['logout'])) {
                 </form>
             </div>
         </div>
+
 
         <script src="/lessons/lesson1/js/main.js"  type="text/javascript"></script>
     </body>
